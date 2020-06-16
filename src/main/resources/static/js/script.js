82198021639c018c90341830767f57574c1becab
Konstantins Lesovojs <konstantins.lesovojs@accenture.com>
2020-06-16 16:59:04 +0300

    Added javascript to call rest api

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,32 @@
 //TODO implement methods calling REST endpoints for buttons in html
+
+      function callCompleteTask(task) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function() {
+                 if (this.readyState == 4 && this.status == 200) {
+                 console.log(this.responseText);
+                 location.reload();
+                 }
+            };
+
+            xhttp.open("PATCH", "/rest/task/setCompleted?id="+task.id , true);
+            xhttp.setRequestHeader("Content-type", "application/json");
+            console.log(JSON.stringify(task));
+            xhttp.send();
+      }
+      function callUncompleteTask(task) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function() {
+                 if (this.readyState == 4 && this.status == 200) {
+                 console.log(this.responseText);
+                 location.reload();
+                 }
+            };
+
+            xhttp.open("PATCH", "/rest/task/setNotCompleted?id="+task.id , true);
+            xhttp.setRequestHeader("Content-type", "application/json");
+            console.log(JSON.stringify(task));
+            xhttp.send();
+      }
